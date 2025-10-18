65a33a0184cb054fad2ea318e7113f4407470747
tahirdibirov <tahirdibirovsecond@gmail.com>
2025-10-18 14:45:23 +0400

    refactor(linked-list)!: convert singly to doubly linked list

--- lib/LinkedList/LinkedList.ts
+++ lib/LinkedList/LinkedList.ts
@@ -3,17 +3,21 @@ import { NotFoundError } from '../exceptions';
 class Node<T> {
   public data: T;
   public next: Node<T> | null;
+  public prev: Node<T> | null;
   constructor(data: T) {
     this.data = data;
     this.next = null;
+    this.prev = null;
   }
 }
 
 export class LinkedList<T> {
   private header: Node<T> | null;
+  private tail: Node<T> | null;
   private size: number;
   constructor() {
     this.header = null;
+    this.tail = null;
     this.size = 0;
   }
   public add(data: T): void {
@@ -21,19 +25,25 @@ export class LinkedList<T> {
     const node = new Node(data);
     if (this.header === null) {
       this.header = node;
+      this.tail = node;
       return;
     } else {
       let current = this.header;
+      let prev = null;
       while (current.next !== null) {
+        prev = current;
         current = current.next;
       }
+      node.prev = prev;
       current.next = node;
+      this.tail = node;
     }
   }
-  public remove(): T {
+  public removeFirst(): T {
     if (this.header !== null) {
       const removed = this.header;
       this.header = this.header.next;
+      if (this.header !== null) this.header.prev = null;
       this.size--;
       return removed.data;
     } else throw new NotFoundError('Cannot remove an empty list');
